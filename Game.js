dac5416ecfd6f2a30f206867a303362a0ec2cf97
ag1910 <58875503+ag1910@users.noreply.github.com>
2020-03-28 16:15:56 +0300

    sayı eklendi

--- Game.js
+++ Game.js
@@ -15,7 +15,16 @@ function changeMarker() {
     this.textContent = '';
   }
 }
-
+var 'O'=plus
+var 'X'=zero
+if zero+zero+zero{
+  var sayı+1
+  window.alert(sayı+" skoruyla Oyun Bitti!");
+}
+if plus+plus+plus{
+  var sayı+1
+  window.alert(sayı+" skoruyla Oyun Bitti!");
+}
 for (var i = 0; i < squares.length; i++) {
   squares[i].addEventListener('click', changeMarker);
 }
